4f89353d1422cc21a251f41a826b0b730c80c730
tsoejima <t.soejimadeveloper@gmail.com>
2021-07-13 14:22:29 +0900

    Initial Commit

---
+++ UrbanLandscapeEyeTracking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UrbanLandscapeEyeTracking
+//
+//  Created by 副島拓哉 on 2021/07/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
