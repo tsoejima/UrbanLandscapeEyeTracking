5de8f7e0d9e1624a0c9e660ebd97cec6224b067c
tsoejima <t.soejimadeveloper@gmail.com>
2021-07-13 17:20:28 +0900

    視線追跡ラベルの追加

--- UrbanLandscapeEyeTracking/ViewController.swift
+++ UrbanLandscapeEyeTracking/ViewController.swift
@@ -16,6 +16,9 @@ class ViewController: EyeTrackViewController {
     var eyeTrackController: EyeTrackController!
     var eyePositionLabel: UILabel!
     var beforeCenterEyeLookAtPoint:CGPoint!
+    var eyePoint:CGPoint!
+    
+    @IBOutlet weak var nowPositionButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +27,7 @@ class ViewController: EyeTrackViewController {
         self.eyeTrackController.onUpdate = { [self] info in
             var labelFlame = eyePositionLabel.frame
             guard let centerEyeLookAtPoint = info?.centerEyeLookAtPoint else { return }
+            eyePoint = centerEyeLookAtPoint
             if beforeCenterEyeLookAtPoint == nil {
                 beforeCenterEyeLookAtPoint = centerEyeLookAtPoint
             }
@@ -41,6 +45,7 @@ class ViewController: EyeTrackViewController {
         self.initialize(eyeTrack: eyeTrackController.eyeTrack)
         self.show()
         self.view.addSubview(eyePositionLabel)
+        self.view.addSubview(nowPositionButton)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -50,16 +55,38 @@ class ViewController: EyeTrackViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
     }
+    var positionIndex = 0
+    @IBAction func nowPositionButton(_ sender: Any) {
+        positionIndex += 1
+        print("ButonPosition",positionIndex,eyePoint!,beforeCenterEyeLookAtPoint)
+        var checkEyePositionLabel: UILabel!
+        checkEyePositionLabel = UILabel()
+        checkEyePositionLabel.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        var eyeFrame = checkEyePositionLabel.frame
+        checkEyePositionLabel.center = self.view.center
+        checkEyePositionLabel.textAlignment = .center
+        checkEyePositionLabel.backgroundColor = .black
+        checkEyePositionLabel.textColor = .white
+        checkEyePositionLabel.text = "Look!"
+        eyeFrame.origin.x = eyePoint.x
+        eyeFrame.origin.y = eyePoint.y
+        checkEyePositionLabel.frame = eyeFrame
+        
+        checkEyePositionLabel.isUserInteractionEnabled = true
+        
+        self.view.addSubview(checkEyePositionLabel)
+    }
+    
     
     func labelSetting() {
-            eyePositionLabel = UILabel()
-            eyePositionLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
-            eyePositionLabel.center = self.view.center
-            eyePositionLabel.textAlignment = .center
-            eyePositionLabel.backgroundColor = .black
-            eyePositionLabel.textColor = .white
-            eyePositionLabel.text = "Look!"
-            eyePositionLabel.isUserInteractionEnabled = true
+        eyePositionLabel = UILabel()
+        eyePositionLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        eyePositionLabel.center = self.view.center
+        eyePositionLabel.textAlignment = .center
+        eyePositionLabel.backgroundColor = .black
+        eyePositionLabel.textColor = .white
+        eyePositionLabel.text = "Look!"
+        eyePositionLabel.isUserInteractionEnabled = true
     }
 
 }
